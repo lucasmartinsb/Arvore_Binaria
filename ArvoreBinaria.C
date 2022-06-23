bddaadd7670b9a5cc731c09b8f43a57802d913b0
Alisson R dos Santos <alirod.santos@gmail.com>
2022-06-23 13:32:22 -0300

    Declaração das Structs

--- ArvoreBinaria.C
+++ ArvoreBinaria.C
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+typedef struct ARVORE
+{
+    char palavra[20];
+    int repeticao;
+    struct ARVORE *left, *right;
+}Arvore;
+
+typedef struct NODE
+{
+    char key;
+    struct NODE *prox;
+}Node;
+
 int main(){
 
     int teste = 10;    
