2904437bb26eade331c833f84f6df60f0f528c66
Alisson <alirod.santos@gmail.com>
2022-06-24 18:14:48 -0300

    Função inserir ordenado inserida no programa

--- ArvoreBinaria.C
+++ ArvoreBinaria.C
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct ARVORE
 {
@@ -11,14 +13,60 @@ typedef struct NODE
 {
     char key;
     struct NODE *prox;
-}Node;
+}Nodo;
+
+Nodo *inicio;
+
+void insereinicio(char key){
+    Nodo *novo;
+
+    novo=(Nodo*)malloc(sizeof(Nodo));
+    novo->key=key;
+    novo->prox=NULL;
+
+    if(inicio==NULL){
+        inicio = novo;
+    }
+    else{
+        novo->prox = inicio;
+        inicio = novo;
+    }
+
+}
+
+void insereordenado(char key){
+    Nodo *aux,*ant,*novo;
+    
+    novo = (Nodo*)malloc(sizeof(Nodo));
+    novo->key = key;
+    novo->prox=NULL;
+    if(inicio==NULL)   //primeiro e unico
+        inicio=novo;
+    else if(key <= inicio->key){  //primeiro
+            novo->prox=inicio;
+            inicio=novo;
+         }
+         else{
+             aux=ant=inicio;   
+             while (aux != NULL && (aux->key) < (novo->key) ){
+                    ant = aux;
+                    aux = aux->prox;
+            }
+            if(aux==NULL){     //no fim
+                ant->prox=novo;
+            }
+            else {  //no meio
+                    ant->prox = novo;
+                    novo->prox = aux;
+            }
+         }
+}
+
 
 int main(){
 
-    int teste = 10;    
-    printf("Fala Rafa");
-
-    teste+=teste;
-
+    insereordenado('b');
+    insereordenado('a');
+    
     return 0;
 }
